2c3203824f3416b616b9598a0a1fa0b5088fdad3
Jake Henderson <jakehenderson@Jakes-MBP.attlocal.net>
2023-06-15 19:24:27 -0400

    generation of grid with valid path of 0s and 1s as obsticles

--- src/graph.rs
+++ src/graph.rs
@@ -2,80 +2,92 @@
 graph algorithms
  */
 mod graph {
-    use rand::Rng;
+    use rand::{Rng, thread_rng, seq::SliceRandom};
 
-    fn generate_valid_grid(w: usize, h: usize) -> Vec<Vec<i8>> {
+    fn generate_valid_grid(w: i32, h: i32) -> Vec<Vec<i32>> {
         let mut rng = rand::thread_rng();
         // 2d array of random 0s and 1s
         let mut grid = vec![vec![0; w as usize]; h as usize];
         for i in 0..h {
             for j in 0..w {
-                grid[i][j] = rng.gen_range(0..=1);
+                grid[i as usize][j as usize] = rng.gen_range(0..=1);
             }
         }
         let start = (0, 0);
         let end = (h - 1, w - 1);
-        // set valid start and end
-        grid[start.0][start.1] = 0;
-        grid[end.0][end.1] = 0;
         // Generate random path
-        let mut current = start;
-        while current != end {
-            let next = get_random_neighbor(current, w, h, &grid, &mut rng);
-            grid[next.0][next.1] = 0;
-            current = next;
-        }
+        generate_valid_path(&mut grid, start, end);
 
         grid
     }
 
-    fn get_random_neighbor(
-        position: (usize, usize),
-        width: usize,
-        height: usize,
-        grid: &Vec<Vec<i8>>,
-        rng: &mut impl Rng,
-    ) -> (usize, usize) {
-        //TODO: this can go on forever
-        // implement validating which neighbors have been visited
-        // implement a left, down, right, up function
-        // and diagonals
-        let mut neighbors = vec![];
-    
-        let x = position.0;
-        let y = position.1;
-    
-        // Check top neighbor
-        if y > 0 && grid[y - 1][x] == 0 {
-            neighbors.push((x, y - 1));
-        }
-    
-        // Check right neighbor
-        if x < width - 1 && grid[y][x + 1] == 0 {
-            neighbors.push((x + 1, y));
-        }
-    
-        // Check bottom neighbor
-        if y < height - 1 && grid[y + 1][x] == 0 {
-            neighbors.push((x, y + 1));
-        }
-    
-        // Check left neighbor
-        if x > 0 && grid[y][x - 1] == 0 {
-            neighbors.push((x - 1, y));
-        }
-    
-        if neighbors.is_empty() {
-            // No available neighbors, return current position
-            position
-        } else {
-            // Return a random neighbor
-            let neighbors_len = neighbors.len();
-            neighbors[rng.gen_range(0..neighbors_len)]
-            // neighbors.choose(rng).unwrap();
+    fn generate_valid_path(grid: &mut Vec<Vec<i32>>, start: (i32, i32), end: (i32, i32)) {
+        /*
+        make start and end valid
+        shuffle the list of next moves
+        if a next move is valid and helps minimize distance, move there
+        if none are alter a move that minimizes distance
+         */
+        // set valid start and end
+        grid[start.0 as usize][start.1 as usize] = 0;
+        grid[end.0 as usize][end.1 as usize] = 0;
+        let mut moves: Vec<(i32,i32)> = vec![
+            (-1, -1), // up left
+            (-1, 0), // up
+            (-1, 1), // up right
+            (0, -1), // left
+            (0, 1), // right
+            (1, -1), // down left
+            (1, 0), // down
+            (1, 1), // down right
+        ];
+
+        let mut rng = thread_rng();
+        let mut current: (i32, i32) = start;
+        /*
+        we want to know if it is in bounds and it decreases the euclidian distance
+         */
+        while current != end {
+            moves.shuffle(&mut rng);
+            for (x_curr, y_curr) in &moves {
+                let next: (i32, i32) = (current.0 as i32 + x_curr, current.1 as i32 + y_curr);
+                // if random diretion is closer and is an obsticle, flip it
+                if is_in_bounds(grid, next) && is_closer(current, next, end) && is_obsticle(grid, next) {
+                        grid[next.0 as usize][next.1 as usize] = 0;
+                        current = next;
+                        break;
+                // if its not an obsticle move forward
+                } else if is_in_bounds(grid, next) && is_closer(current, next, end) {
+                    current = next;
+                    break;
+                }
+            }
         }
     }
 
+    fn is_obsticle(grid: &Vec<Vec<i32>>, next: (i32, i32)) -> bool {
+        grid[next.0 as usize][next.1 as usize] == 1
+    }
+
+    fn is_closer(current:(i32, i32), next: (i32, i32), end: (i32, i32)) -> bool {
+        let next_dist = euclidian_distance(next, end);
+        let curr_dist = euclidian_distance(current, end);
+        next_dist < curr_dist
+    }
+
+    fn euclidian_distance(next: (i32, i32), end: (i32, i32)) -> i32 {
+        let x = (end.0 - next.0 as i32).pow(2);
+        let y = (end.1 - next.1 as i32).pow(2);
+        let distance = (x + y) as f64;
+        distance.sqrt().floor() as i32
+    }
+
+
+    fn is_in_bounds(grid: &Vec<Vec<i32>>, next: (i32, i32)) -> bool {
+        let h = grid.len() as i32;
+        let w = grid[0].len() as i32;
+        next.0 >= 0 && next.0 < h && next.1 >= 0 && next.1 < w
+    }
 
     #[cfg(test)]
     mod graph_test {
@@ -83,11 +95,11 @@ mod graph {
 
         #[test]
         fn test_generate_grid() {
-            let grid = generate_valid_grid(10, 10);
+            let grid = generate_valid_grid(100, 100);
             print_grid(&grid);
         }
 
-        fn print_grid(grid: &Vec<Vec<i8>>) {
+        fn print_grid(grid: &Vec<Vec<i32>>) {
             for row in grid {
                 println!("{:?}", row);
             }
