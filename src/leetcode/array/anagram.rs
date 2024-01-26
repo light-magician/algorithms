379d338ae8e4894b7d0ac959909bc53451a9317d
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-01-25 22:18:31 -0500

    working on array based leetcode problems

---
+++ src/leetcode/array/anagram.rs
@@ -0,0 +1,18 @@
+
+use std::collections:: HashMap;
+
+impl Anagram {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        /*
+        - String is a mutable, growable, heap-allocated string type.
+        It has ownership of the memory it represents and it can be dynamically resized.
+        - str is an immutable, fixed size view into a sequence of UTF-8 bytes.
+        It does not own the underlying data.
+
+         */
+    }
+
+    pub fn count_chars(input: String) -> HashMap<char, usize> {
+        
+    }
+}
