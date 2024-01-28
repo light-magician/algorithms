e0b1bdbcb5f72f64cc3d75d653672ffaca7731a1
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-01-28 13:38:21 -0800

    anagrams in rust

--- src/leetcode/array/anagram.rs
+++ src/leetcode/array/anagram.rs
@@ -1,5 +1,5 @@
 
-use std::collections:: HashMap;
+use std::collections::HashMap;
 
 impl Anagram {
     pub fn is_anagram(s: String, t: String) -> bool {
@@ -8,11 +8,19 @@ impl Anagram {
         It has ownership of the memory it represents and it can be dynamically resized.
         - str is an immutable, fixed size view into a sequence of UTF-8 bytes.
         It does not own the underlying data.
-
          */
+        let c1 = Self::count_chars(s);
+        let c2 = Self::count_chars(t);
+        return c1 == c2;
+
     }
 
     pub fn count_chars(input: String) -> HashMap<char, usize> {
-        
+        let mut counter = HashMap::new();
+        for character in input.chars() {
+            let value = counter.entry(character).or_insert(0);
+            *value += 1;
+        }
+        return counter;
     }
 }
