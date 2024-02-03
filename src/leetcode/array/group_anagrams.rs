aae16373cef5ca82c29684a95478975e346a9e95
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-02-03 17:12:34 -0500

    palindromes

---
+++ src/leetcode/array/group_anagrams.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+impl Solution {
+    /*
+    you can read the letters into a hashmap
+    then make the hashmap a key
+    read each vector into a counter key for another hashmap
+    or you could sort them all and then
+     */
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        
+    }
+}
---
+++ src/leetcode/two_pointer/palindrome.rs
@@ -0,0 +1,41 @@
+
+
+impl Solution {
+    /*
+    A phrase is a palindrome if, after converting all uppercase 
+    letters into lowercase letters and removing all non-alphanumeric 
+    characters, it reads the same forward and backward. 
+    Alphanumeric characters include letters and numbers.
+     */
+    pub fn valid_palindrome(s: String) -> bool {
+        // convert a String to a char vec
+        let chars: Vec<char> = input_string.chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(|c| c.to_ascii_lowercase())
+        .collect();
+
+        let mut left = 0;
+        let mut right = chars.len() -1;
+
+        while left < right {
+            let left_char =  chars[left];
+            let right_char = chars[right];
+            if left_char != right_char {
+                return false;
+            }
+            left += 1;
+            right -= 1;
+        }
+        return true;
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_valid_palindrome() {
+            assert_eq!(valid_palindrome(" ", true));
+        }
+    }
+}
