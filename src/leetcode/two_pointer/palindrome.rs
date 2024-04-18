12b252ffd14dcc30062c0952f06babf25e9b34f7
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-04-18 10:20:30 -0400

    did palindrome again for practice

--- src/leetcode/two_pointer/palindrome.rs
+++ src/leetcode/two_pointer/palindrome.rs
@@ -1,41 +1,30 @@
 
 
-pub mod Palindrome {
-    /*
-    A phrase is a palindrome if, after converting all uppercase 
-    letters into lowercase letters and removing all non-alphanumeric 
-    characters, it reads the same forward and backward. 
-    Alphanumeric characters include letters and numbers.
-     */
-    pub fn valid_palindrome(s: String) -> bool {
-        // convert a String to a char vec
-        let chars: Vec<char> = s.chars()
-        .filter(|c| c.is_ascii_alphanumeric())
-        .map(|c| c.to_ascii_lowercase())
-        .collect();
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        // the chars thing is bs TODO: lookup char in Rust
+        let cleaned: String = s
+            .chars()
+            .filter(|c| c.is_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect();
+
+        // edge cases to waste time
+        if cleaned.is_empty() {
+            return true;
+        }
 
         let mut left = 0;
-        let mut right = chars.len() -1;
+        let mut right = cleaned.len() - 1;
 
         while left < right {
-            let left_char =  chars[left];
-            let right_char = chars[right];
-            if left_char != right_char {
+            if cleaned.chars().nth(left).unwrap() != cleaned.chars().nth(right).unwrap() {
                 return false;
             }
             left += 1;
             right -= 1;
         }
-        return true;
-    }
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn test_valid_palindrome() {
-            assert_eq!(valid_palindrome(" ".to_string()), true);
-        }
+        true
     }
 }
