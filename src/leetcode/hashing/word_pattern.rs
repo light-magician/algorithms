ae1f99f8d2b1c78aeffd26e90644cbf08bcef7fa
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-04-20 20:00:20 -0400

    word pattern matching

--- src/leetcode/hashing/word_pattern.rs
+++ src/leetcode/hashing/word_pattern.rs
@@ -3,31 +3,33 @@ use std::collections::HashMap;
 impl Solution {
     pub fn word_pattern(pattern: String, s: String) -> bool {
         // need to know how to split or something
-        let mut pattern_nums: Vec<usize> = Vec::new();
+        let mut pattern_nums: usize = 1;
         let mut pattern_map: HashMap<char, usize> = HashMap::new();
-        let mut s_nums: Vec<usize> = Vec::new();
-        let mut s_map: HashMap<&str, usize> = HashMap::new();
         let mut unique: usize = 0;
+
         for c in pattern.chars() {
             match pattern_map.get(&c) { // take ref but not responsible
                 Some(&val) => {
-                    pattern_nums.push(val);
+                    pattern_nums = (pattern_nums * 10) + val;
                 },
                 None => {
-                    pattern_nums.push(unique);
+                    pattern_nums = (pattern_nums * 10) + unique;
                     pattern_map.insert(c, unique);
                     unique += 1;
                 }
             }
         }
+
         unique = 0;
+        let mut s_nums: usize = 1;
+        let mut s_map: HashMap<&str, usize> = HashMap::new();
         for token in s.split_whitespace() { // gives a string slice &str
             match s_map.get(&token) { // take ref but not responsible
                 Some(&val) => {
-                    s_nums.push(val);
+                    s_nums = (s_nums * 10) + val;
                 },
                 None => {
-                    s_nums.push(unique);
+                    s_nums = (s_nums * 10) + unique;
                     s_map.insert(token, unique);
                     unique += 1;
                 }
