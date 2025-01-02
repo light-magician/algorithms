75fe9b3d22bbc0705c016810ee8929046517b720
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2025-01-02 11:55:51 -0500

    feat: added greatest common denominator of substring problem

---
+++ src/blind_75/README.md
@@ -0,0 +1,6 @@
+
+## link to the leetcode 75 
+https://leetcode.com/studyplan/leetcode-75/
+
+## neetcode's Blind 75 
+https://neetcode.io/practice
--- src/blind_75/array_string/mod.rs
+++ src/blind_75/array_string/mod.rs
@@ -29,9 +29,42 @@ pub fn merge_alternately(word1: String, word2: String) -> String {
 /// Given two strings str1 and str2, return the largest string
 /// x such that x divides both str1 and str2.
 pub fn gcd_of_strings(str1: String, str2: String) -> String {
-    // ABCABC ABC ->  xABC
-    // ABABAB ABAB -> AB
-    // LEET CODE -> (empty)
+    // Start with the shorter string as potential GCD
+    let mut gcd = if str1.len() <= str2.len() {
+        str1.clone()
+    } else {
+        str2.clone()
+    };
+    
+    // Loop through possible GCD lengths
+    while !gcd.is_empty() {
+        if string_is_divided(&str1, &gcd) 
+        && string_is_divided(&str2, &gcd) {
+            return gcd; 
+        }
+        // Remove last character to try shorter potential GCD
+        gcd.truncate(gcd.len() - 1);
+    }
+    
+    // Return empty string if no common divisor found
+    String::new()
+}
+
+fn string_is_divided(strx: &String, test: &String) -> bool {
+    // Check if the string length is divisible by test length
+    if strx.len() % test.len() != 0 {
+        return false;
+    }
+    
+    // Check if the test string repeats perfectly to form strx
+    for i in (0..strx.len()).step_by(test.len()) {
+        let slice = String::from(&strx[i..i+test.len()]);
+        if &slice != test {
+            return false;
+        }
+    }
+    
+    true
 }
 
 #[cfg(test)]
@@ -48,5 +81,18 @@ mod array_string_tests {
         word1 = String::from("!@#$%^&*");
         word2 = String::from("");
         assert_eq!("!@#$%^&*", merge_alternately(word1, word2));
+    }
+
+    #[test]
+    fn test_gcd_of_string() {
+        let test_cases = [
+            (String::from("ABCABC"), String::from("ABC"), String::from("ABC")),
+            (String::from("ABABAB"), String::from("AB"), String::from("AB")),
+            (String::from("TAUXXTAUXXTAUXXTAUXXTAUXX"), String::from("TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"), String::from("TAUXX")),
+        ];
+        
+        for (str1, str2, ans) in test_cases.iter() {
+            assert_eq!(gcd_of_strings(str1.clone(), str2.clone()), ans.clone());
+        }
     }
 }
