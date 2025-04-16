7edc396959ff265598954a074a7b68fe75f6865f
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2025-04-16 08:58:38 -0700

    feat: redundant roundabout way to call fcuntions within threads

--- os.c/create_thread.c
+++ os.c/create_thread.c
@@ -3,9 +3,17 @@
 #include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>
 
-void *mythread(void *arg) {
-  printf("%s\n", (char *)arg);
+// take pointer to function and execute
+void *mythread(void *fn) {
+  fn;
+  return NULL;
+}
+
+// take pointer to string and print it
+void *print(void *str) {
+  printf("%s\n", (char *)str);
   return NULL;
 }
 
@@ -13,8 +21,8 @@ int main(int argc, char *argv[]) {
   pthread_t p1, p2;
   int rc;
   printf("main: begin\n");
-  pthread_create(&p1, NULL, mythread, "A");
-  pthread_create(&p2, NULL, mythread, "B");
+  pthread_create(&p1, NULL, mythread, print("A"));
+  pthread_create(&p2, NULL, mythread, print("B"));
   // join waits for the threads to finish
   pthread_join(p1, NULL);
   pthread_join(p2, NULL);
